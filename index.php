0af20e37436fab8e4f76d3fb7a59441687ad6f9d
elwaters <carlos.wyss@gmail.com>
2013-09-15 10:16:04 +0200

    incluir intro.html, centrar body al 80%

--- index.php
+++ index.php
@@ -8,11 +8,19 @@
   <title>Machoculto | Lugar de palabra y figura</title>
 
 </head>
+<LINK href="styles.css" rel="stylesheet" type="text/css">
 
 <body>
 
   <h1>Machoculto</h1>
   <h2>Lugar de palabra y figura</h2>
+
+
+  <?php
+  include "intro.html";
+  ?>
+
+
 
 </body>
 </html>
